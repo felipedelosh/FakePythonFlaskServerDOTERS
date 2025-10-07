c43ce5f46cb13e964cc50b69f1c951f06ebb56ab
felipedelosh <doctorfhernandez@hotmail.com>
2025-10-07 17:52:03 -0500

    DB.user

--- app/Database/context.py
+++ app/Database/context.py
@@ -14,6 +14,39 @@ class DbContext:
         self.db_path = db_path
         self.conn = sqlite3.connect(self.db_path, check_same_thread=False)
         self.cursor = self.conn.cursor()
+        self.create_tables()
+
+    def create_tables(self):
+        try:
+            with self.conn:
+                self.conn.execute('''
+                    CREATE TABLE IF NOT EXISTS users (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        language TEXT NOT NULL,
+                        enrollingSponsor TEXT NOT NULL,
+                        enrollingChannel TEXT NOT NULL,
+                        phoneNumber TEXT NOT NULL,
+                        email TEXT NOT NULL UNIQUE,
+                        password TEXT NOT NULL,
+                        activationUrl TEXT NOT NULL,
+                        subscribeToNotifications INTEGER NOT NULL DEFAULT 1 CHECK (subscribeToNotifications IN (0,1)),
+                        firstName TEXT NOT NULL,
+                        lastName TEXT NOT NULL,
+                        title TEXT,
+                        dateOfBirth TEXT,
+                        gender TEXT NOT NULL,
+                        residentCountry TEXT NOT NULL,
+                        created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
+                    );
+                ''')
+                self.conn.commit()
+                # self.conn.execute('''
+                #     CREATE TABLE IF NOT EXISTS ??? (
+                #         ...
+                #     )
+                # ''')
+        except sqlite3.Error as e:
+            pass
 
     def get_connection(self):
         return self.conn
