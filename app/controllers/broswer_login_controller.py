26f1e404a04db2c23f84110d8c6e66722517b79c
felipedelosh <doctorfhernandez@hotmail.com>
2025-10-17 16:31:22 -0500

    WIP:ObtainTokens:id_token

--- app/controllers/broswer_login_controller.py
+++ app/controllers/broswer_login_controller.py
@@ -5,6 +5,7 @@ from app.helpers.response import error_response
 from app.repositories.login_repository import LoginRepository
 from app.services.login_service import LoginService
 from app.UseCases.login_use_case import LoginUseCase
+from app.helpers.fakeTokenizer import create_token
 
 def browser_login():
     return render_template("login.html")
@@ -31,10 +32,18 @@ def browser_login_post():
         if not response:
             return error_response("INVALID USR AND PASS", "UNAUTHORIZED", 401)
         
+        claims = {
+            "sub": email,
+            "email": email,
+            "role": "member",
+            "uid": str(repo.get_by_email(email)["id"])
+        }
+        id_token = create_token(claims)
+        
         data = {
             "access_token": "sqY5n6QcgvSNX7lAf78d2A6hJxop-439Rhr0cqO4nuD",
             "expires_in": "86400",
-            "id_token": "eyJhbGciOiJSUzI1NiIsInR5cCI6...",
+            "id_token": id_token,
             "refresh_token": "0WsteyOWU14Ew-t-vf7KEKOg3SSr6ak1PJMWV9n8abg",
             "scope": "openid offline_access email profile",
             "token_type": "Bearer",
@@ -46,7 +55,7 @@ def browser_login_post():
         merged_qs = {**existing_qs, **data}
         new_query = urlencode(merged_qs, doseq=True)
         final_url = urlunparse(parsed._replace(query=new_query))
-        
+
         return redirect(final_url, code=302)
     except:
         return error_response("Server Error", "INTERNAL_ERROR", 500)
